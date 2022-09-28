87b949e8aae257dccf72d6cbc1f250626ee4d101
Amaan-Ali-Prodige <amaan.ali@prodigedigital.com>
2022-09-28 19:01:17 +0530

    added number animation functionality

---
+++ public/index.js
@@ -0,0 +1,38 @@
+let counterWrapper = document.getElementById('counterWrapper')
+let counter = []
+let counts = []
+let flag = [true, true, true, true, true]
+let upto = [0, 0, 0, 0, 0]
+for (let i = 0; i < counterWrapper.children.length; i++) {
+	counter[i] = document.getElementById(`counter${i}`)
+	counter[i] = counter[i]?.innerHTML * 1
+}
+const updated = pos => {
+	if (flag[pos]) {
+		document.getElementById(`counter${pos}`).innerHTML = ++upto[pos]
+	}
+	if (upto[pos] >= counter[pos]) {
+		clearInterval(counts[pos])
+		// document.getElementById(`counter${pos}`).innerHTML = counter[pos]
+		flag[pos] = false
+	}
+}
+
+window.onscroll = () => {
+	scrollFunc()
+}
+
+let headerElem = document.getElementById('headerElem')
+const scrollFunc = () => {
+	// console.log("Console ~ file: index.html ~ line 253 ~ scrollFunc ~ document.documentElement.scrollTop", document.documentElement.scrollTop)
+	document.documentElement.scrollTop > 120
+		? (headerElem.style.backgroundColor = '#374055e6')
+		: (headerElem.style.backgroundColor = 'transparent')
+	if (document.documentElement.scrollTop > 490) {
+		for (let i = 0; i < counter.length; i++) {
+			counts[i] = setInterval(() => {
+				updated(i)
+			})
+		}
+	}
+}
